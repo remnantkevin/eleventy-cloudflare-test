a84ef1851d9922ceb77bc65807bbb7d4f31fc1ac
Kevin Elliott <skiimilk@gmail.com>
2022-03-11 15:24:46 +0800

    Update

--- functions/api/first.js
+++ functions/api/first.js
@@ -8,9 +8,11 @@ export async function onRequest(context) {
 
     const { country, regionCode, region } = request.cf;
 
-    const holidayData = await env.DATA.get(`${country}:${regionCode}`);
+    const holidayData = await env.DATA.get(`${country}:${regionCode}`, {
+      type: "json",
+    });
 
-    return new Response(holidayData);
+    return new Response(JSON.stringify(holidayData[1]));
   } catch (e) {
     return new Response(JSON.stringify(e, Object.getOwnPropertyNames(e)));
   }
