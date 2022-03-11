52316480cb4e2821b9ffb65ea52714012c25d239
Kevin Elliott <skiimilk@gmail.com>
2022-03-11 15:16:25 +0800

    Update

--- functions/api/first.js
+++ functions/api/first.js
@@ -4,15 +4,11 @@ export async function onRequest(context) {
     const {
       request, // same as existing Worker API
       env, // same as existing Worker API
-      params, // if filename includes [id] or [[path]]
-      waitUntil, // same as ctx.waitUntil in existing Worker API
-      next, // used for middleware or to fetch assets
-      data, // arbitrary space for passing data between middlewares
     } = context;
 
     const { country, regionCode, region } = request.cf;
 
-    const holidayData = await DATA.get(`${country}:${regionCode}`);
+    const holidayData = await env.DATA.get(`${country}:${regionCode}`);
 
     return new Response(holidayData);
   } catch (e) {
